a656cd872009e870b3eb689839a926eddf7f40c1
Humacsson <johannaslund@protonmail.com>
2024-06-17 09:59:25 +0900

    Fix input redirects etc

--- agent/scripts/run.sh
+++ agent/scripts/run.sh
@@ -1,9 +1,15 @@
 litellm --model ollama/llama3 --api_base http://host.docker.internal:11434 &
-P1=$!
 
-python agent.py &
-P2=$!
+echo -n 'Waiting for litellm'
+# Use weget to check if the URL is up: 
+#   --spider speeds up the check by not downloading the content
+#   2> dev/null hides error message
+until wget --spider localhost:4000 2> /dev/null
+do
+  echo -n '.'
+  sleep 1
+done
 
-# Make sure all running processes can be stopped with ctrl-c
-wait $P1 $P2
+echo 'Connection established! Running agent...'
 
+python agent.py
